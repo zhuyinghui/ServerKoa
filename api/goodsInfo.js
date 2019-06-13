53f77d5e0063327f1ac6cff3523873b5a94de4d9
zhuyinghui <404993465@qq.com>
2019-06-13 10:13:30 +0800

    1

--- api/goodsInfo.js
+++ api/goodsInfo.js
@@ -25,54 +25,53 @@ router.get('/',async (ctx)=>{
 	ctx.body=result;
 });
 
-//商品添加
-
-let multer=require('koa-multer');
-let storage = multer.diskStorage({
-	//文件保存路径
-	destination: function (req, file, cb) {
-	  cb(null, './uploads/')
-	},
-	filename:function(req,file,cb){
-	var fileFormat = (file.originalname).split(".");
-	cb(null,Date.now() + "." + fileFormat[fileFormat.length - 1]);
-	}
-  })
-  //加载配置
-  let upload = multer({ storage: storage });
-  //路由
-router.post('/',upload.array('file'),async(ctx,next)=>{
-	// let data=ctx.req.body;
-	// let images=[];
-	// for(let item of ctx.req.files){
-	// 	images.push(item.filename);
-	// }
-	// const a=await DB.limit('sellerInfo',data.seller);
-	// if(a){
-	// 	data.goods.seller_id=data.seller.phone;
-	// 	data.goods.images=images;
-	// 	let message=await DB.add('goodsInfo',data.goods);
-	// 	ctx.body=message;
-	// }else{
-	// 	ctx.body='无卖家权限';
-	// }
-	ctx.body={
-		body:ctx.req.body,
-		files:ctx.req.files
-	}
-})
-
-// router.post('/',async (ctx)=>{
-// 	let data=ctx.request.body;
-// 	const a=await DB.limit('sellerInfo',data.seller);
-// 	if(a){
-// 		data.goods.seller_id=data.seller.phone;
-// 		let message=await DB.add('goodsInfo',data.goods);
-// 		ctx.body=message;
-// 	}else{
-// 		ctx.body='无卖家权限';
+// //商品添加
+// let multer=require('koa-multer');
+// let storage = multer.diskStorage({
+// 	//文件保存路径
+// 	destination: function (req, file, cb) {
+// 	  cb(null, './uploads/')
+// 	},
+// 	filename:function(req,file,cb){
+// 	var fileFormat = (file.originalname).split(".");
+// 	cb(null,Date.now() + "." + fileFormat[fileFormat.length - 1]);
 // 	}
-// });
+//   })
+//   //加载配置
+//   let upload = multer({ storage: storage });
+//   //路由
+// router.post('/',upload.array('file'),async(ctx,next)=>{
+// 	// let data=ctx.req.body;
+// 	// let images=[];
+// 	// for(let item of ctx.req.files){
+// 	// 	images.push(item.filename);
+// 	// }
+// 	// const a=await DB.limit('sellerInfo',data.seller);
+// 	// if(a){
+// 	// 	data.goods.seller_id=data.seller.phone;
+// 	// 	data.goods.images=images;
+// 	// 	let message=await DB.add('goodsInfo',data.goods);
+// 	// 	ctx.body=message;
+// 	// }else{
+// 	// 	ctx.body='无卖家权限';
+// 	// }
+// 	ctx.body={
+// 		body:ctx.req.body
+// 		// files:ctx.req.files
+// 	}
+// })
+
+router.post('/',async (ctx)=>{
+	let data=ctx.request.body;
+	const a=await DB.limit('sellerInfo',data.seller);
+	if(a){
+		data.goods.seller_id=data.seller.phone;
+		let message=await DB.add('goodsInfo',data.goods);
+		ctx.body=message;
+	}else{
+		ctx.body='无卖家权限';
+	}
+});
 
 //商品删除
 router.delete('/',async (ctx)=>{
